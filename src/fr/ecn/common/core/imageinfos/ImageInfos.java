771cec1c8ba05a0301308d8439d06cfd0cdd38e2
Jérôme Vasseur <jerome.vasseur@jhome.fr>
2011-11-25 15:17:04 +0100

    Moving ImageInfos to commons

---
+++ src/fr/ecn/common/core/imageinfos/ImageInfos.java
@@ -0,0 +1,133 @@
+package fr.ecn.common.core.imageinfos;
+
+import java.io.Serializable;
+import java.util.List;
+
+import fr.ecn.common.core.geometry.Point;
+
+public class ImageInfos implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	protected String path = null;
+
+	protected Coordinate latitude  = null;
+	protected Coordinate longitude = null;
+	
+	protected Double orientation = null;
+	
+	protected List<Point> vanishingPoints = null;
+	
+	protected Double yHorizon = null;
+	
+	protected List<Face> faces = null;
+	
+	/**
+	 * 
+	 */
+	public ImageInfos() {
+		super();
+	}
+
+	public ImageInfos(String path) {
+		super();
+		this.path = path;
+	}
+
+	/**
+	 * @param path the path to set
+	 */
+	public void setPath(String path) {
+		this.path = path;
+	}
+
+	public String getPath() {
+		return path;
+	}
+	
+	/**
+	 * @return the latitude
+	 */
+	public Coordinate getLatitude() {
+		return latitude;
+	}
+
+	/**
+	 * @param latitude the latitude to set
+	 */
+	public void setLatitude(Coordinate latitude) {
+		this.latitude = latitude;
+	}
+
+	/**
+	 * @return the longitude
+	 */
+	public Coordinate getLongitude() {
+		return longitude;
+	}
+
+	/**
+	 * @param longitude the longitude to set
+	 */
+	public void setLongitude(Coordinate longitude) {
+		this.longitude = longitude;
+	}
+
+	public Double getOrientation() {
+		return orientation;
+	}
+	
+	public void setOrientation(Double orientation) {
+		this.orientation = orientation;
+	}
+
+	/**
+	 * @return the vanishingPoints
+	 */
+	public List<Point> getVanishingPoints() {
+		return vanishingPoints;
+	}
+
+	/**
+	 * @param vanishingPoints the vanishingPoints to set
+	 */
+	public void setVanishingPoints(List<Point> vanishingPoints) {
+		this.vanishingPoints = vanishingPoints;
+	}
+	
+	/**
+	 * @return the yHorizon
+	 */
+	public Double getYHorizon() {
+		return yHorizon;
+	}
+
+	/**
+	 * @param yHorizon the yHorizon to set
+	 */
+	public void setYHorizon(Double yHorizon) {
+		this.yHorizon = yHorizon;
+	}
+
+	/**
+	 * @return the faces
+	 */
+	public List<Face> getFaces() {
+		return faces;
+	}
+
+	/**
+	 * @param faces the faces to set
+	 */
+	public void setFaces(List<Face> faces) {
+		this.faces = faces;
+	}
+
+	@Override
+	public String toString() {
+		return "ImageInfos [path=" + path + ", latitude=" + latitude + ", longitude=" + longitude
+				+ ", orientation=" + orientation + "]";
+	}
+}
